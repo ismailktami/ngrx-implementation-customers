ce1ff5f83bf61be4218da610c110af314bd194e9
ismailKtami <ktamiismail@gmail.com>
2019-10-06 03:04:56 +0100

    make betterssssss

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
 const path =require('path')
-const jsonServer = require('json-server');
 
-app.use(express.static(__dirname + '/dist/reduximplementation'))
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname + '/dist/reduximplementation/index.html'))
+app.use(express.static(__dirname + "/dist/reduximplementation"))
+app.get('*',function (req,res) {
+  res.sendFile(path.join(__dirname+'/dist/reduximplementation/index.html'))
 })
-app.use('/api',jsonServer.router('db.json'))
 app.listen(process.env.PORT || 4200)
-app.listen(3000)
