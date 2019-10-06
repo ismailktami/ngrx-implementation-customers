9432d0d0bd9822d643083fb41cec046054f3650f
ismailKtami <ktamiismail@gmail.com>
2019-10-06 20:58:54 +0100

    better

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const path =require('path')
-
 app.use(express.static(__dirname + "/dist/reduximplementation"))
 app.get('*',function (req,res) {
   res.sendFile(path.join(__dirname+'/dist/reduximplementation/index.html'))
